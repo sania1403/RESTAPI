0db0fac23a2e288901c081817411aa2da878c66a
sania1403 <106600747+sania1403@users.noreply.github.com>
2024-07-09 14:31:52 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,38 @@
+package com.demo.binding;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="COURSE_DTLS")
+public class Course {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	private String name;
+	private Double price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
